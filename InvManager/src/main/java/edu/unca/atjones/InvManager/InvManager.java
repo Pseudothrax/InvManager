7d8b14d2b627c4267f60529d0f413ad36092abad
Asa Jones <atjones@unca.edu>
2012-12-06 16:25:47 -0500

    added upgrade ability

--- InvManager/src/main/java/edu/unca/atjones/InvManager/InvManager.java
+++ InvManager/src/main/java/edu/unca/atjones/InvManager/InvManager.java
@@ -1,8 +1,11 @@
 package edu.unca.atjones.InvManager;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,7 +16,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class InvManager extends JavaPlugin {
     
 	HashMap<String,HashMap<String,Inventory>> inventories;
+	HashMap<String,HashMap<Integer,String>> routes;
 	HashMap<String,Integer> blocks;
+	HashMap<String,Integer> upgrades;
 	
 	InvManagerLogger logger;
 	
@@ -24,17 +29,27 @@ public final class InvManager extends JavaPlugin {
 		logger.info("plugin enabled");
 		
 		inventories = new HashMap<String,HashMap<String,Inventory>>();
+		routes = new HashMap<String,HashMap<Integer,String>>();
 		blocks = new HashMap<String,Integer>();
+		upgrades = new HashMap<String,Integer>();
 		
         saveDefaultConfig();
         
-        // Create the SampleListener
+        // Create Listener
         new InvManagerListener(this);
         
-        // set the command executor for sample
-        this.getCommand("invopen").setExecutor(new InvManagerOpenCommandExecutor(this));
-        this.getCommand("invroute").setExecutor(new InvManagerRouteCommandExecutor(this));
-        this.getCommand("invadmin").setExecutor(new InvManagerAdminCommandExecutor(this));
+        // set the command executors
+        this.getCommand("invcreate").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invgrant").setExecutor(new InvManagerCommandExecutor(this));        
+        this.getCommand("invhelp").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invlist").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invopen").setExecutor(new InvManagerCommandExecutor(this));        
+        this.getCommand("invremove").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invrename").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invroute").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invroutes").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invupgrade").setExecutor(new InvManagerCommandExecutor(this));
+        this.getCommand("invupgrades").setExecutor(new InvManagerCommandExecutor(this));
         
     }
     
@@ -58,8 +73,7 @@ public final class InvManager extends JavaPlugin {
     	owner.openInventory(inv);
     }
     
-    public void createInventory(Player owner,String type,String name) throws InvManagerException {
-    	String title = name;
+    public void createInventory(Player owner,String name,String type) throws InvManagerException {
     	if(name == null) throw new InvManagerException("Must provide a name for this inventory.");
     	int size = 0;
     	if(type != null) {
@@ -67,18 +81,134 @@ public final class InvManager extends JavaPlugin {
 	    	else if(type.equalsIgnoreCase("medium")) size = 18;
 	    	else if(type.equalsIgnoreCase("large")) size = 27;
 	    	else throw new InvManagerException(type + "is not a valid inventory type");
-    	} else throw new InvManagerException("Must provide an inventory type");
+    	} else size = 9;
     	
-    	Inventory newInv = Bukkit.getServer().createInventory(owner, size, title);
     	HashMap<String,Inventory> invs;
     	if(inventories.containsKey(owner.getName())) {
     		invs = inventories.get(owner.getName());
     	} else {
     		invs = new HashMap<String,Inventory>();
     	}
-		if(invs.containsKey(title)) throw new InvManagerException("An inventory by that name already exists");
-		invs.put(title, newInv);
+		if(invs.containsKey(name)) throw new InvManagerException("An inventory by that name already exists");
+    	Inventory newInv = Bukkit.getServer().createInventory(owner, size, name);
+		invs.put(name, newInv);
 		inventories.put(owner.getName(), invs);
     }
+    
+    public void upgradeInventory(Player owner, String name) throws InvManagerException {
+    	if(inventories.containsKey(owner.getName())) {
+    		HashMap<String,Inventory> invs = inventories.get(owner.getName());
+    		if(invs.containsKey(name)) {
+    			Inventory i = invs.get(name);
+    			if(i.getSize() != 27){
+    				int size = i.getSize() + 9;
+    				Inventory I = Bukkit.getServer().createInventory(owner, size, name);
+    				I.setContents(i.getContents());
+    				invs.put(name, I);
+    				inventories.put(owner.getName(), invs);
+    			} else throw new InvManagerException("Inventory cannot be upgraded (Too large)");
+    		}
+    		else throw new InvManagerException("Inventory not found.");
+    	} 
+    	else throw new InvManagerException("Player has no inventories.");
+    }
+    
+    public void removeInventory(Player owner, String name) throws InvManagerException {
+    	if(inventories.containsKey(owner.getName())) {
+    		HashMap<String,Inventory> invs = inventories.get(owner.getName());
+    		if(invs.containsKey(name)) {
+    			invs.remove(name);
+    		}
+    		else throw new InvManagerException("Inventory not found.");
+    	} 
+    	else throw new InvManagerException("Player has no inventories.");
+    }
+    
+    public Set<String> listInventories(Player owner) throws InvManagerException {
+    	if(inventories.containsKey(owner.getName())) {
+    		HashMap<String,Inventory> invs = inventories.get(owner.getName());
+    		Set<String> names = invs.keySet();
+    		return names;
+    	} 
+    	else throw new InvManagerException("Player has no inventories.");
+    }
+    
+    public void grantUpgrade(Player owner,int num) {
+    	String playerName = owner.getName();
+    	if(upgrades.containsKey(playerName)) {
+    		int count = upgrades.get(playerName) + num;
+    		upgrades.put(playerName,count);
+    	}
+    	else upgrades.put(playerName,num);
+    }
+    
+    public boolean hasUpgrade(Player owner) {
+    	String playerName = owner.getName();
+    	if(upgrades.containsKey(playerName)) {
+    		if(upgrades.get(playerName) > 0) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public int numUpgrades(Player owner) {
+    	String playerName = owner.getName();
+    	if(upgrades.containsKey(playerName)) {
+    		return upgrades.get(playerName);
+    	}
+    	return 0;
+    }
+    
+    public void useUpgrade(Player owner) throws InvManagerException {
+    	String playerName = owner.getName();
+    	if(upgrades.containsKey(playerName)) {
+    		int count = upgrades.get(playerName);
+    		if( count > 0) {
+    			count--;
+        		upgrades.put(playerName,count);
+    		}
+    		else throw new InvManagerException("Player has no upgrades");
+    	}
+    	else throw new InvManagerException("Player has no upgrades");
+    }
+    
+    public void addRoute(Player owner, String item, String inventory) throws InvManagerException {
+		HashMap<Integer,String> playerRoutes;
+		HashMap<String,Inventory> playerInventories;
+		String player = owner.getName();
+		
+		if(routes.containsKey(player)) {
+			playerRoutes = routes.get(player);
+		} else {
+			playerRoutes = new HashMap<Integer,String>();
+		}
+		
+		if(inventories.containsKey(player)) {
+			playerInventories = inventories.get(player);
+		} else throw new InvManagerException("No Inventories found!");
+		
+		if(playerInventories.containsKey(inventory)) {
+			//First see if item is the item name string
+			Material itemMaterial = Material.matchMaterial(item);
+			
+			//If that doesn't work try it as an integer item id
+			if(itemMaterial == null) {
+				try {
+					itemMaterial = Material.getMaterial(Integer.parseInt(item));
+				} catch(NumberFormatException e) {
+					throw new InvManagerException("Unrecognized Item");
+				}	
+			}
+			
+			//If the item still hasn't been recognized throw an exception
+			if(itemMaterial == null) throw new InvManagerException("Unrecognized Item");
+			
+			//Otherwise add route
+			playerRoutes.put(itemMaterial.getId(),inventory);
+			routes.put(player,playerRoutes);
+		}
+				
+	}
 
 }
--- InvManager/src/main/java/edu/unca/atjones/InvManager/InvManagerListener.java
+++ InvManager/src/main/java/edu/unca/atjones/InvManager/InvManagerListener.java
@@ -1,13 +1,19 @@
 package edu.unca.atjones.InvManager;
 
+import java.util.HashMap;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import edu.unca.atjones.MoreEvents.InventoryAddEvent;
 
 /*
  * This is a sample event listener
@@ -44,17 +50,57 @@ public class InvManagerListener implements Listener {
     	//increment the player's block count
     	Player p = event.getPlayer();
     	String playerName = p.getName();
-    	if(plugin.blocks.containsKey(playerName)) {
-    		plugin.blocks.put(playerName, plugin.blocks.get(playerName) + 1);
-    	} else {
-    		plugin.blocks.put(playerName, 1);
+    	if(p.hasPermission("inventory.reward")) {
+    		if(plugin.blocks.containsKey(playerName)) {
+        		int count = plugin.blocks.get(playerName) + 1;
+        		if(count == 10) {
+        			plugin.blocks.put(playerName, 0);
+        			plugin.grantUpgrade(p,1);
+        			p.sendMessage("You've received an upgrade for your hard work!");
+        			p.sendMessage("  Type /invupgrade <inventory name> to upgrade the size of one of your inventories.");
+        			p.sendMessage("  Type /invcreate <inventory name> to create a new small inventory.");
+        			p.sendMessage("  Type /invhelp for additional help.");
+        		}
+        		else {
+        			plugin.blocks.put(playerName, count);
+        		}
+        	} else {
+        		plugin.blocks.put(playerName, 1);
+        	}
     	}
-    	p.sendMessage("Blocks Broken: " + String.valueOf(plugin.blocks.get(playerName)));
+    	
+
     }
     
     @EventHandler
-    public void onPickupItem(PlayerPickupItemEvent event) {
+    public void onInventory(InventoryEvent event) {
+    	plugin.logger.info("Inventory Event");
+    	
+    }
+    
+    @EventHandler
+    public void onInventoryAdd(InventoryAddEvent event) {
     	//route the item according to assigned routing rules
+    	Player p = (Player)event.getPlayer();
+    	String player = p.getName();
+    	ItemStack item = event.getItemStack();
+    	int itemId = item.getData().getItemTypeId();
+    	String itemName = item.getData().getItemType().toString();
+    	
+    	if(plugin.inventories.containsKey(player)) {
+    		if(plugin.routes.containsKey(player)) {
+    			HashMap<Integer,String> playerRoutes = plugin.routes.get(player);
+        		HashMap<String,Inventory> playerInventories = plugin.inventories.get(player);
+        		if(playerRoutes.containsKey(itemId)) {
+        			String destName = playerRoutes.get(itemId);
+        			if(playerInventories.containsKey(destName)) {
+        				Inventory dest = playerInventories.get(destName);
+        				event.setCancelled(true);
+        				dest.addItem(item);
+        			}
+        		};
+    		}
+    	}
     }
     
     @EventHandler
